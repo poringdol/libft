05b249c92577b34b80f57b08a7bf51620cc6f855
poringdol <poringdol@gmail.com>
2020-03-28 21:20:01 +0000

    ft_lstmap add NULL protect

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -7,13 +7,12 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list *res;
 
 	fresh = NULL;
-	if (lst != NULL && f != NULL)
-	{
-		fresh = ft_lstnew(lst->content, lst->content_size);
-		fresh = f(fresh);
-		res = fresh;
-	}
-	while (lst->next != NULL && f != NULL)
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	fresh = ft_lstnew(lst->content, lst->content_size);
+	fresh = f(fresh);
+	res = fresh;
+	while (lst->next != NULL)
 	{
  		lst = lst->next;
 		fresh->next = ft_lstnew(lst->content, lst->content_size);
