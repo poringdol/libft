dd35f504adbaaed68ae0dcb6f44555adf1b3cddb
Admin <admin@Mac-Admin.local>
2020-03-31 21:12:57 +0300

    ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,21 +6,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list *fresh;
 	t_list *res;
 
-	fresh = NULL;
 	if (lst == NULL || f == NULL)
 		return (NULL);
-	if (!(fresh = ft_lstnew(lst->content)))
+	if (!(res = ft_lstnew(f(lst->content))))
 		return (NULL);
-	fresh = f(fresh);
-	res = fresh;
-	while (lst->next != NULL)
+	lst = lst->next;
+	while (lst != NULL)
 	{
- 		lst = lst->next;
-		if (!(fresh->next = ft_lstnew(lst->content)))
+		if (!(fresh = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&res, del);
 			return (NULL);
-		fresh->next = f(fresh->next);
-		fresh = fresh->next;
+		}
+	ft_lstadd_back(&res, fresh);
+	lst = lst->next;	
 	}
-	del(fresh);
 	return (res);
 }
