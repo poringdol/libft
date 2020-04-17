60b967db5d897d4aa92b551e9dc4ec9161d3040c
CDdoma <sashe@bk.ru>
2020-04-17 20:14:51 +0000

    fix overflow check for ft_calloc

--- sources/ft_calloc.c
+++ sources/ft_calloc.c
@@ -5,17 +5,19 @@
 void	*ft_calloc(size_t n, size_t size)
 {
 	char	*buf;
-	size_t	max_size;
+	size_t	buf_size;
 
-	max_size = (FT_SIZE_MAX / size >= n) ? size * n : FT_SIZE_MAX;
-	buf = (void *)malloc(max_size);
+	if (FT_SIZE_MAX / size < n)
+		return (NULL);
+	buf_size = size * n;
+	buf = (void *)malloc(buf_size);
 	if (buf == NULL)
 		return (NULL);
-	buf[max_size - 1] = '\0';
-	while (max_size > 0)
+	buf[buf_size - 1] = '\0';
+	while (buf_size > 0)
 	{
-		max_size--;
-		buf[max_size] = '\0';
-	}	
+		buf_size--;
+		buf[buf_size] = '\0';
+	}
 	return ((void *)buf);
 }
