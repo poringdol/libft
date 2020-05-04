02b6508c41a3c1a3a2a703fcf8484f2e53c48b9a
Admin <admin@Mac-Admin.local>
2020-05-04 05:11:30 +0300

    fix lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -20,9 +20,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
-	if (!(res = ft_lstnew(f(lst->content))))
-		return (NULL);
-	lst = lst->next;
+	res = NULL;
 	while (lst != NULL)
 	{
 		if (!(fresh = ft_lstnew(f(lst->content))))
